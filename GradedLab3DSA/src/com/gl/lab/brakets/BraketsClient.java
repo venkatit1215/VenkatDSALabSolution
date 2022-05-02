38d42bd5bdbfaf3851b8fa2ee0e4b5582aedd53d
LAPTOP-3BBK4LB9\LEKSHITH & JENVITH <venkatit1215@gmail.com>
2022-05-02 07:36:26 +0530

    Checking length is even or odd.

--- GradedLab3DSA/src/com/gl/lab/brakets/BraketsClient.java
+++ GradedLab3DSA/src/com/gl/lab/brakets/BraketsClient.java
@@ -4,24 +4,29 @@ public class BraketsClient {
 
 	public static void main(String[] args) {
 		//String str = "([[{}]])";
-	    String str = "([[{}]]))";
+	   //String str = "([[{}]]]";
+	    String str = "([[{}]])]";
 		int length = str.length();
 		boolean isBalanced = true;
-		for (int i = 1; i <= length / 2; i++) {
-			char startBraket = str.charAt(i - 1);
-			char endBraket = str.charAt(length - i);
-			if (!isBalanced)
-				break;
-			if('(' == startBraket && ')' != endBraket)
-				isBalanced = false;
-			if('[' == startBraket && ']' != endBraket)
-				isBalanced = false;
-			if('{' == startBraket && '}' != endBraket)
-				isBalanced = false;
+		if(length%2!=0) {
+			isBalanced = false;
+		} else {
+			for (int i = 1; i <= length / 2; i++) {
+				char startBraket = str.charAt(i - 1);
+				char endBraket = str.charAt(length - i);
+				if (!isBalanced)
+					break;
+				if('(' == startBraket && ')' != endBraket)
+					isBalanced = false;
+				if('[' == startBraket && ']' != endBraket)
+					isBalanced = false;
+				if('{' == startBraket && '}' != endBraket)
+					isBalanced = false;
 
+			}
 		}
+		
 		if (isBalanced) {
-
 			System.out.println("The entered String has Balanced Brackets");
 		} else {
 			System.out.println("The entered Strings do not contain Balanced Brackets");
